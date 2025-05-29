164b5b5524a6a0569b7e175f779605f1cb311974
Tamil2031 <tamil21112001>
2025-05-29 13:26:10 +0530

    first commit

---
+++ lint-test-ios/LintTestIOSsApp.swift
@@ -0,0 +1,17 @@
+//
+//  lint_test_iosApp.swift
+//  lint-test-ios
+//
+//  Created by Krithik Roshan on 29/05/25.
+//
+
+import SwiftUI
+
+@main
+struct LintTestIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ lint-test-iosTests/linttestiostests.swift
@@ -0,0 +1,17 @@
+//
+//  lint_test_iosTests.swift
+//  lint-test-iosTests
+//
+//  Created by Krithik Roshan on 29/05/25.
+//
+
+import Testing
+@testable import lint_test_ios
+
+struct linttestiosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
